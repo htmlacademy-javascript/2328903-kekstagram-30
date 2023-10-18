95f24f5446d17a1a04a1f81fe53de1546a1acd31
Nostradamus <54951070+Nostradamusx06@users.noreply.github.com>
2023-10-18 20:08:16 +0300

    Нужно больше функций #2
    * Добавление функций
    * Корректирует тесты
    
    ---------
    
    Co-authored-by: Arthur Baileys Li <arthurlitovko@gmail.com>

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+function checkStringLength(inputString, maxLength) {
+  return inputString.length <= maxLength;
+}
+
+function isPalindrome(inputString) {
+  const cleanedString = inputString.toLowerCase().replace(/\s/g, '');
+
+  return cleanedString === cleanedString.split('').reverse().join('');
+}
+
+function extractAndSumDigits(input) {
+  if (typeof input === 'number') {
+    return input;
+  }
+
+  if (typeof input !== 'string') {
+    return NaN;
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const digit = parseInt(input[i], 10);
+
+    if (!isNaN(digit)) {
+      result = result * 10 + digit;
+    }
+  }
+
+  return result === 0 ? NaN : Math.abs(result);
+}
+
+export {checkStringLength, isPalindrome, extractAndSumDigits}
